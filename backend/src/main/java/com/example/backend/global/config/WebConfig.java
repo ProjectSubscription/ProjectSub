b1ffd26b2c9efd3f481c6c618e0f5f6c1b3220e6
simmina <s1mm1n6h@gmail.com>
2026-01-16 16:28:16 +0900

    chore: init feature/oauth-login-v2

---
+++ backend/src/main/java/com/example/backend/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.backend.global.config;
+
+import com.example.backend.auth.resolver.LoginMemberArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> resolvers
+    ) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+}
