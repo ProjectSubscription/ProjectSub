9dc6658d729d93e88f9f376e9dc5a1e1881a7fa2
jaehyun <wogusrla77@naver.com>
2026-01-18 16:09:10 +0900

    fix: Promotion 도메인 삭제

--- backend/src/main/java/com/example/backend/broadcast/promotion/repository/PromotionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.broadcast.promotion.repository;
-
-import com.example.backend.broadcast.promotion.entity.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-
-}
