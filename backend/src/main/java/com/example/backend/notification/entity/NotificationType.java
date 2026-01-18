2aab91853082d0146fa5135006c97ae56fb4c2a3
jaehyun <wogusrla77@naver.com>
2026-01-18 16:07:21 +0900

    fix: NotificationType 추가

--- backend/src/main/java/com/example/backend/notification/entity/NotificationType.java
+++ backend/src/main/java/com/example/backend/notification/entity/NotificationType.java
@@ -4,5 +4,7 @@ public enum NotificationType {
     NEW_CONTENT, // 새로운 콘텐츠
     NEWS_LETTER, // 뉴스레터
     EVENT, // 이벤트
-    COMMENT // 댓글
+    COMMENT, // 댓글
+    CHANNEL, // 채널
+    COUPON_MY_PAGE // 내 쿠폰 목록
 }
